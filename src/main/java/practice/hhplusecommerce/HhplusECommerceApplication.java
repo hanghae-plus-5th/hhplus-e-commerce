ae0636de1b8471c48e861fac6a0afdf9a5d819dd
백현명 <whitewise95@users.noreply.github.com>
2024-07-04 21:12:55 +0900

    feat: 프로젝트 셋팅

---
+++ src/main/java/practice/hhplusecommerce/HhplusECommerceApplication.java
@@ -0,0 +1,13 @@
+package practice.hhplusecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusECommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HhplusECommerceApplication.class, args);
+  }
+
+}
