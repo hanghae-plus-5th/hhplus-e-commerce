0a1c05641c4b39cd2ae2deb9be74a114eb82d678
백현명 <whitewise95@users.noreply.github.com>
2024-07-19 02:04:07 +0900

    fix: swagger Header에 jwt 담을 수 있도록 수정

--- src/main/java/practice/hhplusecommerce/common/config/SwaggerConfig.java
+++ src/main/java/practice/hhplusecommerce/common/config/SwaggerConfig.java
@@ -4,24 +4,37 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
+  
   @Bean
   public OpenAPI openAPI() {
-    return new OpenAPI()
-        .components(new Components())
-        .info(apiInfo());
-  }
-
-  private Info apiInfo() {
-    return new Info()
+    Info info = new Info()
         .title("Springdoc Swagger")
         .description("Springdoc을 사용한 Swagger UI")
         .version("1.0.0")
         .license(new License().name("Apache 2.0").url("http://springdoc.org"));
+
+    // SecuritySecheme명
+    String jwtSchemeName = "jwtAuth";
+    // API 요청헤더에 인증정보 포함
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+    // SecuritySchemes 등록
+    Components components = new Components()
+        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+            .name(jwtSchemeName)
+            .type(SecurityScheme.Type.HTTP) // HTTP 방식
+            .scheme("bearer")
+            .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+    return new OpenAPI()
+        .info(info)
+        .addSecurityItem(securityRequirement)
+        .components(components);
   }
 }
